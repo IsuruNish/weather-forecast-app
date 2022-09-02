059968684fe9c8b2eb5cefc39a57758e33efc0c5
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2022-09-02 12:37:05 +0530

    Connect With The Weather API Using Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9d98d05b9005fbf9533b9d30bb3fc292`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=colombo&appid=9d98d05b9005fbf9533b9d30bb3fc292`;
+
+  const searchLoocation = () => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    });
+
+    setLocation("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input />
+      <button onClick={searchLoocation}> add </button>
+      <h1> {data.name} </h1>
+      {data.main ? <h1>{data.main.temp.toFixed()} °F</h1> : null}
+      {data.weather ? <h1>{data.weather[0].main}</h1> : null}
+      {data.main ? <h1>{data.main.feels_like.toFixed()} °F</h1> : null}
+      {data.main ? <h1>{data.main.humidity} %</h1> : null}
+      {data.wind ? <h1>{data.wind.speed.toFixed()} MPH</h1> : null}
     </div>
   );
 }
